74353ba997e54f58c15efacfd642bd3d6b59bc60
Aneudy Mota <aneudy.motacatalino@gmail.com>
2016-10-30 23:23:54 -0400

    Fixed typo on ssl check file

--- recipes/ssl_config.rb
+++ recipes/ssl_config.rb
@@ -12,7 +12,7 @@ script 'Create Self-Signed Certificate' do
 		openssl req -nodes -new -sha256 -newkey rsa:2048 -keyout /etc/nginx/ssl/server_name.key -out /etc/nginx/ssl/server_name.csr -subj "/C=US/ST=Michigan/L=Kalamazoo/O=Compucorp/OU=DevOps/CN=localhost"
 		openssl x509 -req -sha256 -days 365 -in /etc/nginx/ssl/server_name.csr -signkey /etc/nginx/ssl/server_name.key -out /etc/nginx/ssl/server_name.crt
 		openssl dhparam -out /etc/nginx/ssl/dhparams.pem 2048 \
-		&& touch #{check_files}/created_slef_signed_certificate
+		&& touch #{check_files}/created_self_signed_certificate
 	EOH
-	not_if do ::File.exists?("#{check_files}/created_slef_signed_certificate") end
+	not_if do ::File.exists?("#{check_files}/created_self_signed_certificate") end
 end
