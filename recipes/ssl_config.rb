76f6be6ea25ba7fda79d40b62e38bfd633dca631
Aneudy Mota <aneudy.motacatalino@gmail.com>
2016-10-30 22:19:27 -0400

    fixed -in server_name.csr path

--- recipes/ssl_config.rb
+++ recipes/ssl_config.rb
@@ -10,7 +10,7 @@ script 'Create Self-Signed Certificate' do
 	interpreter "bash"
 	code <<-EOH
 		openssl req -nodes -new -sha256 -newkey rsa:2048 -keyout /etc/nginx/ssl/server_name.key -out /etc/nginx/ssl/server_name.csr -subj "/C=US/ST=Michigan/L=Kalamazoo/O=Compucorp/OU=DevOps/CN=localhost"
-		openssl x509 -req -sha256 -days 365 -in server.csr -signkey /etc/nginx/ssl/server_name.key -out /etc/nginx/ssl/server_name.crt
+		openssl x509 -req -sha256 -days 365 -in /etc/nginx/ssl/server.csr -signkey /etc/nginx/ssl/server_name.key -out /etc/nginx/ssl/server_name.crt
 		openssl dhparam -out /etc/nginx/ssl/dhparams.pem 2048 \
 		&& touch #{check_files}/created_slef_signed_certificate
 	EOH
